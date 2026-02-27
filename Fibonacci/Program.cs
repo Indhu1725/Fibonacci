c7266272fc36b81e883f21140d104b79d157a2cd
Indhu1725 <deepthi.chowdary00@gmail.com>
2026-02-27 19:06:17 +0530

    Add project files.

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter num: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int p = 0, q = 1;
+
+            Console.WriteLine("Fibonacci Series:");
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write(p + " ");
+
+                int r = p + q;
+                p = q;
+                q = r;
+            }
+        }
+    }
+}
